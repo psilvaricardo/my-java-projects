e453f5aceebbb77e7f8db14c60ac91f2bdb40532
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-10-11 18:35:58 -0500

    Adding Java Streams examples

--- DataStructuresAlgorithms/src/com/prep/java8/streams/StreamsExample1.java
+++ DataStructuresAlgorithms/src/com/prep/java8/streams/StreamsExample1.java
@@ -31,4 +31,36 @@ public class StreamsExample1 {
 
     }
 
+    /**
+     * Streams are using functional programming heavily and are supported starting in Java 8
+     *
+     * Stream Source: they can be created from Collections, Lists, Sets, ints, longs, doubles, arrays, lines of a file.
+     *
+     * They make heavy use of lambda expressions
+     * ParallelStreams make it very easy can leverage parallel processing, multi-thread operations
+     * A stream pipeline consists of a source, followed by zero or more intermediate operations
+     * and a terminal operation.
+     *
+     * Intermediate operations: filter, map or sort return a stream, so we can chain multiple intermediate operations.
+     * Zero or more intermediate operations are allowed. Order matter for large datasets: filter first, then sort or map.
+     * For very large datasets, use ParallelStream to enable multiple threads. Intermediate operations include:
+     *
+     *      anyMatch()      flatpam()
+     *      distinct()      map()
+     *      filter()        skip()
+     *      findFirst()     sorted()
+     *
+     * Terminal operations: forEach, Collect, reduce are either void or return a non-stream result.
+     *
+     *      forEach(): applies the same operations to each element, for example print.
+     *      collect(): will save all the elements into a collection
+     *
+     * other operations reduce the stream to a single summary element:
+     *
+     *      count()         min()
+     *      max()           reduce()
+     *      summaryStatistics()
+     *
+     **/
+
 }
